fd0806b29e2deb4583f131c55253cc9562e7cbdb
Ludovic <ludovicdebever0@gmail.com>
2024-12-22 14:41:48 +0100

    improve(hero): improve aurora colors

--- src/components/ui/aurora/aurora.tsx
+++ src/components/ui/aurora/aurora.tsx
@@ -7,9 +7,9 @@ export const Aurora = () => {
       <div
         className={cn(
           `
-            [--white-gradient:repeating-linear-gradient(100deg,var(--white)_0%,var(--white)_70%,var(--transparent)_10%,var(--transparent)_12%,var(--white)_16%)]
+            [--white-gradient:repeating-linear-gradient(100deg,var(--indigo)_0%,var(--indigo)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--indigo)_16%)]
             [--dark-gradient:repeating-linear-gradient(100deg,var(--black)_0%,var(--black)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--black)_16%)]
-            [--aurora:repeating-linear-gradient(100deg,var(--pink-500)_10%,var(--amber-300)_15%,var(--pink-300)_20%,var(--violet-200)_25%,var(--pink-400)_30%)]
+            [--aurora:repeating-linear-gradient(100deg,var(--pink-500)_10%,var(--indigo-700)_15%,var(--pink-600)_20%,var(--violet-200)_25%,var(--pink-700)_30%)]
             [background-image:var(--white-gradient),var(--aurora)]
             dark:[background-image:var(--dark-gradient),var(--aurora)]
             [background-size:300%,_200%]
